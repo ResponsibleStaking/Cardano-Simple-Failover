a2f4d7b5961975d5034c1bd7ddc24d94a73195f2
ResponsibleStaking <79546146+ResponsibleStaking@users.noreply.github.com>
2022-02-23 22:09:10 +0100

    update

--- script/makeStandby.sh
+++ script/makeStandby.sh
@@ -1,3 +1,3 @@
 #Copy the iptables line below for every relay which connects to your node and place it's IP in "YOUR-RELAY-IP" and uncomment it
 echo "MAKING STANDBY"
-iptables -I INPUT -s YOUR-RELAY-IP -p tcp --dport YOUR-NODE-PORT -j DROP
+#iptables -I INPUT -s YOUR-RELAY-IP -p tcp --dport YOUR-NODE-PORT -j DROP
