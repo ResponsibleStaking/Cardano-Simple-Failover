67f867cb13604ed900805f3c1f593abc17a4b789
ResponsibleStaking <79546146+ResponsibleStaking@users.noreply.github.com>
2022-02-25 21:43:32 +0100

    SendTip

--- script/makeActive.sh
+++ script/makeActive.sh
@@ -4,3 +4,7 @@
 #Copy the iptables line below for every relay which connects to your node and place it's IP in "YOUR-RELAY-IP" and uncomment it
 echo "--- MAKING ACTIVE ---"
 #iptables -D INPUT -s YOUR-RELAY-IP -p tcp --dport YOUR-NODE-PORT -j DROP
+
+
+#If you use CNTools SendTip you can activate it on the Failover if getting Active
+#systemctl start cnode-cncli-ptsendtip.service
--- script/makeStandby.sh
+++ script/makeStandby.sh
@@ -4,3 +4,7 @@
 #Copy the iptables line below for every relay which connects to your node and place it's IP in "YOUR-RELAY-IP" and uncomment it
 echo "--- MAKING STANDBY ---"
 #iptables -I INPUT -s YOUR-RELAY-IP -p tcp --dport YOUR-NODE-PORT -j DROP
+
+
+#If you use CNTools SendTip you can activate it on the Failover if getting Active
+#systemctl stop cnode-cncli-ptsendtip.service
